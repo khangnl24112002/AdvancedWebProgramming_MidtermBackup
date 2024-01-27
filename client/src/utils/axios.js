f977eaf314db5cb19455f7a51418ab1377c77102
Khang Nguyen Nhat <khangnl24112002@gmail.com>
2024-01-27 11:35:03 +0700

    Change server

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,40 +1,39 @@
 import axios from "axios";
 const getToken = () => JSON.parse(localStorage.getItem("token"));
 export const axiosInstance = axios.create({
-    baseURL:
-        "https://advanced-web-programming-midterm-production.up.railway.app/",
-    timeout: 10000,
-    headers: {
-        Authorization: `Bearer ${getToken()}`,
-        post: {
-            "Content-Type": "application/json",
-        },
-        put: {
-            "Content-Type": "application/json",
-        },
+  baseURL: "advanced-web-programming-midterm-production-90aa.up.railway.app/",
+  timeout: 10000,
+  headers: {
+    Authorization: `Bearer ${getToken()}`,
+    post: {
+      "Content-Type": "application/json",
     },
+    put: {
+      "Content-Type": "application/json",
+    },
+  },
 });
 
 axios.interceptors.request.use(
-    (request) => {
-        console.log(request);
-        // Edit request config
-        return request;
-    },
-    (error) => {
-        console.log(error);
-        return Promise.reject(error);
-    }
+  (request) => {
+    console.log(request);
+    // Edit request config
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
 );
 
 axios.interceptors.response.use(
-    (response) => {
-        console.log(response);
-        // Edit response config
-        return response;
-    },
-    (error) => {
-        console.log(error);
-        return Promise.reject(error);
-    }
+  (response) => {
+    console.log(response);
+    // Edit response config
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
 );
